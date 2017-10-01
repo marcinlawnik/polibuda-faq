2e3de6fa3f900b73b4ccaeee8dd7b44d83f0619b
Marcin Ławniczak <marcin@lawniczak.me>
2017-10-01 21:36:51 +0200

    Add travis config

---
+++ setup-dist.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Note the set -ev at the top.
+# The -e flag causes the script to exit as soon as
+# one command returns a non-zero exit code. This
+# can be handy if you want whatever script you have
+# to exit early. It also helps in complex installation
+# scripts where one failed command wouldn’t otherwise
+# cause the installation to fail.
+set -ev
+
+pip install grip
